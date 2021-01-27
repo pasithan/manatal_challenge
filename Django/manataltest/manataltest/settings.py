263287faa80c4806a0a971de8d575a5bdb352427
pasithan <pasit.han@hotmail.com>
2021-01-27 21:27:29 +0700

    Edit files directory to solve deployment problem

--- Django/manataltest/manataltest/settings.py
+++ Django/manataltest/manataltest/settings.py
@@ -131,12 +131,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, '/staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, '/static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 #  Add configuration for static files storage using whitenoise
